fd3e6e09673d9239608d983bd44ed95428d1beb4
daishadavis <daishadavis84@gmail.com>
2024-01-06 21:25:34 -0500

    Format and comment changes

--- bicycles.py
+++ bicycles.py
@@ -1,6 +1,6 @@
 #Accessing elements in a list using index
-#bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-#print(bicycles[0].title())
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title()) 
 
 #example of 1 and 3 index
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
@@ -16,19 +16,18 @@ bicycles = ['trek', 'connondale', 'redline', 'specialized']
 message = "My first bicycle was a {}".format(bicycles[0].title())
 print(message)
 
-names = ['gabby', 'ella', 'carlos', 'mack']
+names = ['gabby', 'ella', 'carlos', 'emily']
 print(names[0])
 print(names[1])
 print(names[2])
 print(names[3])
 
-names = ['gabby', 'ella', 'carlos', 'mack']
 print("Hello {} I'm so happy to see you!".format(names[0]))
 print("Hello {} I'm so happy to see you!".format(names[1]))
 print("Hello {} I'm so happy to see you!".format(names[2]))
-print("Hello {} I'm so happy to see you!".format(names[3]))
+print(f"Hello {names[3].title()} I'm so happy to see you!")
 
 transportation = ['car', 'motorcycle', 'plane']
 print("My {} is the most expensive thing I own".format(transportation[0]))
 print("{} are my favorite mode of transportation".format(transportation[1]))
-print("I would love to fly an {} one day.".format(transportation[2]))
+print(f"I would love to fly an {transportation[2].title()} one day.")
--- motorcycles.py
+++ motorcycles.py
@@ -1,74 +1,80 @@
-#motorcycles = ['honda', 'yasmaha', 'suzuki']
-#print(motorcycles)
+motorcycles = ['honda', 'yasmaha', 'suzuki']
+print(motorcycles)
 
-#motorcycles[0] = 'ducati'
-#print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
 
-#motorcycles = ['honda', 'yasmaha', 'suzuki']
-#motorcycles.append('ducati')
-#print(motorcycles)
+motorcycles = ['honda', 'yasmaha', 'suzuki']
+motorcycles.append('ducati')
+print(motorcycles)
 
-#motorcycles = []
-#motorcycles.append('honda')
-#motorcycles.append('yamaha')
-#motorcycles.append('suzuki')
-#print(motorcycles)
+#Adding names to a list 
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
 
-#motorcycle = ['honda', 'yasmaga', 'suzuki']
-#motorcycle.insert(0, 'ducati')
-#print(motorcycle)
+#Adding an element to a specific positon of a list
+motorcycle = ['honda', 'yasmaga', 'suzuki']
+motorcycle.insert(0, 'ducati')
+print(motorcycle)
 
-#motorcycle = ['honda', 'yamaha', 'suzuki']
-#print(motorcycle)
+#removing a item from a list 
+motorcycle = ['honda', 'yamaha', 'suzuki']
+print(motorcycle)
 
-#del motorcycle[0]
-#print(motorcycle)
+del motorcycle[0]
+print(motorcycle)
 
-#motorcycle = ['honda', 'yamaha', 'suzuki']
-#del motorcycle[1]
-#print(motorcycle)
+motorcycle = ['honda', 'yamaha', 'suzuki']
+del motorcycle[1]
+print(motorcycle)
 
-#motorcycles = ['honda', 'yamaha', 'suzuki']
-#print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
 
-#popped_motorcycles = motorcycles.pop()
-#print(motorcycles)
-#print(popped_motorcycles)
+#removing the last element fomr the list and return the value
+popped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
 
-#motorcycles = ['honda', 'yamaha', 'suzuki']
-#last_owned = motorcycles.pop()
-#print("The last motorcycle I owned was a {}".format(last_owned.title()))
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a {}".format(last_owned.title()))
 
-#motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles = ['honda', 'yamaha', 'suzuki']
 
-#first_owned = motorcycles.pop(0)
-#print("The first motorcycle I owned was a {}".format(first_owned.title())) 
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a {}".format(first_owned.title())) 
 
-#motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
-#print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
 
-#motorcycles.remove('ducati')
-#print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
 
-#motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
-#print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
 
-#too_expensive = 'ducati'
-#motorcycles.remove(too_expensive)
-#print(motorcycles)
-#print("{} is too expensive for me.".format(too_expensive.title()))
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("{} is too expensive for me.".format(too_expensive.title()))
 
 
 guess_list = ['Elon Musk', 'Bill Gates', 'Jay Z']
 print("Welcome to my dinner party{}".format(guess_list))
 print()
 
-print("{} can't make it to the party".format(guess_list[2]))#adding who won't be coming 
-del guess_list[2]#removing the person who won't be coming
+#adding who won't be coming
+print("{} can't make it to the party".format(guess_list[2]))
+del guess_list[2]
 print()
 
-guess_list.insert(2, "Allen Turing")#adding the person that will be coming
-print("Welcome to my dinner party {}".format(guess_list))#update of who will be coming
+#adding the person who will be coming 
+guess_list.insert(2, "Allen Turing")
+print("Welcome to my dinner party {}".format(guess_list))
 print()
 
 print("We have found a bigger table for people to join us.")
@@ -92,8 +98,9 @@ print("I'm sorry Elon you can't come")
 guess_list.pop(1)#Removing bill gates from the list
 print("I'm sorry Bill you can't come")
 
+#deleting both items off the list
 print("{0} and {1} are still invited to the party".format(guess_list[0], guess_list[1]))
-del guess_list[0]#deleting both litems off the list
+del guess_list[0]
 del guess_list[0]
 
 print(guess_list)
