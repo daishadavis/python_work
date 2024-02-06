84cc078f6d4f0f9d348c5ea602ab1adae0551ad7
daishadavis <daishadavis84@gmail.com>
2024-02-05 19:19:27 -0500

    Fromat changes

--- Integers.py
+++ Integers.py
@@ -1,22 +1,22 @@
-#print(5+3)
-#print(5-3)
-#print(5*3)
-#print(5/3)
-#print(3**3)
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print(3**3)
 
-#print(2 + 3*4)
-#print((2 + 3)*4)
+print(2 + 3*4)
+print((2 + 3)*4)
 
-#print(0.2 + 0.2)
-#print(2 * 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
 
-#print(4 / 2)
+print(4 / 2)
 
-#universe_age = 14_000_000_000
-#print(universe_age)
+universe_age = 14_000_000_000
+print(universe_age)
 
-#x, y, z = 0, 0, 0
-#print(x, y, z)
+x, y, z = 0, 0, 0
+print(x, y, z)
 
 #praticing addtion, subtraction, mulitplaction and divsion.
 print(5 + 3)
