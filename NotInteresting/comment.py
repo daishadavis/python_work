437d70e7770f2deb79a39c2a4f3fff233df556de
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2024-02-10 03:03:29 -0500

    This will make good reference notes

--- NotInteresting/comment.py
+++
@@ -1,5 +0,0 @@
-#Say hello to everyone.
-#print("Hello Python people")
-
-import this
-"The zen of Python, by Tim Peters"
--- NotInteresting/voting.py
+++
@@ -1,7 +0,0 @@
-age = 17
-if age >= 18:
-    print("You are old enough to vote!")
-    print("Have youn registered to vote yet?")
-else:
-    print("Sorry, you are too young to vote.")
-    print("Please register to vote when you are 18 or older")
