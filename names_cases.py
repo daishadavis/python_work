1bd74675f1d7f1941142f196c7c9b25c6588058b
daishadavis <daishadavis84@gmail.com>
2024-02-10 09:46:38 -0500

    folders

--- names_cases.py
+++
@@ -1,4 +0,0 @@
-first_name = "Daisha"
-last_name = "Davis"
-full_name = f"{first_name} {last_name}"
-print(full_name)
--- variables_simple_data_types/name_cases.py
+++ variables_simple_data_types/name_cases.py
@@ -1,5 +1,5 @@
 name = "Eric Smith"
-print("Hello {}, would you like to learn some python today?".format(name))
+print(f"Hello {name}, would you like to learn some python today?")
 
 print(name.upper())
 print(name.lower())
@@ -8,11 +8,14 @@ print(name.title())
 print('Albert Einstein once said, "A person who never made a mistake never tried anything new" ')
 famous_person = "Albert Einstein"
 message = "'A person who never made a mistake never tried anything new'"
-print("{0} once said, {1}".format(famous_person, message))
+print(f"{famous_person} once said, {message}.")
 
 name = "\tJames Bond\n "
 print(name.lstrip())
 print(name.rstrip())
 print(name.strip())
 
-#Practing using strings and variables in python 
+first_name = "Daisha"
+last_name = "Davis"
+full_name = f"{first_name} {last_name}"
+print(full_name)
